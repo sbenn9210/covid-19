2d2e97a528ee822ea8cf5de3cdaa67dfc761d08f
Shawn Benny <sbenn9210@gmail.com>
2020-04-19 18:34:38 -0500

    Dont remove stuff you dont need to be removing

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,16 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import Typography from "@material-ui/core/Typography";
 
 function Summary() {
-    useEffect(() => {
-        axios.get('https://api.covid19api.com/summary')
-        .then(response => console.log(response))
-    })
+  useEffect(() => {
+    axios
+      .get("https://api.covid19api.com/summary")
+      .then((response) => response)
+      .then((data) => console.log(data));
+  });
 
-    return (
-        <div></div>
-    )
+  return (
+    <Container>
+      <Typography>Shawn wekwleeklekwelwkelekeklwkwkwewlekwel</Typography>
+    </Container>
+  );
 }
 
+export default Summary;
 
-export default Summary
+const Container = styled.div`
+  flexGrow: 1,
+    padding: theme.spacing(3),
+`;
